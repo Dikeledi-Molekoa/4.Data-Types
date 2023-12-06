7bf12d64de601ebaf1574643c521d919d8d1831b
A252431 <A252431@LSBSA-C26NHW3>
2023-12-06 14:13:45 +0200

    Add project files.

---
+++ 4.Data Types/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name = "John";
+            int age = 35;
+            bool isTall = false;
+            double weight = 80.2;
+
+            /*For the smallest least precise decimal use float,
+             *For more precise use decimal eg for money,
+             *In moast cases double will do just fine
+             */
+
+            Console.WriteLine(name + " is " + age + " and he weighs " + weight + "kg");
+            
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
